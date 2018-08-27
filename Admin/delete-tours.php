ca8d4e50ee477abc99d3b5a20407c431533b90c3
Nashrif Mahmud <nashrifmahmud19@gmail.com>
2018-08-28 03:02:48 +0600

    admin part

---
+++ Admin/delete-tours.php
@@ -0,0 +1,17 @@
+<?php
+
+//including the database connection file
+    include("config.php");
+    //connect_db();
+//getting id of the data from url
+    $tour_id = $_GET['tour_id'];
+
+//deleting the row from table // actually not deleting it just unlinking from the result
+    //$result = mysqli_query($conn,"delete from test WHERE id='$id'");
+    $result = mysqli_query($conn,"update tours set deletedAt= CURRENT_TIMESTAMP where tour_id= '$tour_id';");
+	//close_db();
+//redirecting to the display page (listdata.php in our case)
+    header("Location:all-tours.php");
+
+?>
+
